0c382d3121cd06b5960c60b288f75df72ebc06a4
phoethingyan-tech <phoethingyan.tech@gmail.com>
2025-05-11 14:53:01 +0630

    update admin panel

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\FrontController::class, 'blog'])->name('blog');
 Route::get('/detail/{id}', [App\Http\Controllers\FrontController::class, 'detail'])->name('detail');
+
+Route::group(['prefix'=>'backend', 'as'=>'backend.'], function() {
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'dashboard'])->name('dashboard');
+});
 Auth::routes();
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
